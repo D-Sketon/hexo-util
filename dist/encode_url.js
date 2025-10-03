5f074015c8a2567035e1a152e34dcc30f173bc7b
D-Sketon <2055272094@qq.com>
2025-10-03 19:26:08 +0800

    chore: update dist

--- dist/encode_url.js
+++ dist/encode_url.js
@@ -1,8 +1,14 @@
 "use strict";
 const url_1 = require("url");
 const querystring_1 = require("querystring");
+const PROTOCOL_RE = /^[a-z0-9.+-]+:/i;
+const hasProtocolLikeNode = (str) => {
+    if (typeof str !== 'string')
+        throw new TypeError('url must be a string');
+    return PROTOCOL_RE.test(str.trim());
+};
 const encodeURL = (str) => {
-    if ((0, url_1.parse)(str).protocol) {
+    if (hasProtocolLikeNode(str)) {
         const parsed = new URL(str);
         // Exit if input is a data url
         if (parsed.origin === 'null')
